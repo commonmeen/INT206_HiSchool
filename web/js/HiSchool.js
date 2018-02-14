79c7bed7db0639424edfd1a9a901b294e4b9d9ff
Erth <panot1997@hotmail.com>
2018-02-14 16:52:28 +0700

    [Edit] HiSchool.js

--- web/js/HiSchool.js
+++ web/js/HiSchool.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
     $('div.container').fadeIn(1000).removeClass('container');
 });
+$(document).ready(function () {
+    $('div.row-fadein').fadeIn(2000).removeClass('row-fadein');
+});
